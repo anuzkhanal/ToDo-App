582204d9b01f382760572416638fd03da45d0db7
Anuj Khanal <anuj.khanal@dreambroker.fi>
2023-08-10 10:51:57 +0300

    Create basic To-Do List App

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do List App</h1>
+      <TodoForm addTask={addTask} />
+      <TodoList tasks={tasks} />
     </div>
   );
 }
